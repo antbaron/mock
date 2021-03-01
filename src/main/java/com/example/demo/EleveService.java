b689f4142886ed29332a247bd8c06be081a45010
mostafaza <mohatrt99@gmail.com>
2021-03-01 15:45:28 +0100

    ajout de la moyenne1

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -13,10 +13,17 @@ public class EleveService {
 	public boolean isPositiveNote(int note) {
 		return 0 < note;
 	}
-	
-	
-	public int mobouhou() {
-		return 0;
+
+
+	 public int mobouhou(int note) {
+		if(isPositiveNote(note) && note<=20){
+			Eleve eleve = new Eleve("mostafa",note);
+			eleveRepository.addEleve(eleve);
+			note = eleveRepository.findMean("mostafa");
+		}else{
+			note =  -1;
+		}
+		return note;
 	}
 	
 }
