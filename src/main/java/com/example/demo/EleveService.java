5b3f06a8f4ce57637c6ff5493fde1d9f3830edab
Sébastien Busschot <sebastien.pro.busschot@gmail.com>
2021-03-01 14:27:37 +0100

    just do it!

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -14,7 +14,23 @@ public class EleveService {
 		return 0 < note;
 	}
 	
-	public void sebuc4() {
+	public int sebuc4(int note) {
+		
+		// verifier que c'est une note
+		if(note < 0 && note > 20) {
+			// return -1 si incorrect
+			return -1;
+		} else {
+			// sinon créer un élève Seb
+			String name = "seb";
+			Eleve seb = new Eleve(name, note);
+			// appeller add eleve de eleve repository
+			eleveRepository.addEleve(seb);
+			
+			// retourner l'entier resulat de la methode findmean
+			return eleveRepository.findMean(name);
+			
+		}
 		
 	}
 	
