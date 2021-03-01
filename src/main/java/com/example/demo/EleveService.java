d9bec9de86bb806bf689a45aa8dccd07ca6f5b5b
ThomasCpr <adressethomas.c@gmail.com>
2021-03-01 14:28:16 +0100

    premier ajout de code

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -14,9 +14,21 @@ public class EleveService {
 		return 0 < note;
 	}
 	
-	public int tcapriolombardi() {
-		return 0;
+	// 1/ ajouter un entier en param appelé note
+	// 2/ Vérifier que cet entier est bien une note 
+	// 3/ /Si la note n'est pas correcte -> return -1
+	// 3.bis // si la note est correcte -6> créer un éléève + appelé la métthode addEleève du repo.
+	// 4/ retourner l'entier , qui est le résultat de la function findMean
+	public int tcapriolombardi(int note) {
+		if(isPositiveNote(note)) {
+			Eleve e = new Eleve("Thomas", note);
+			eleveRepository.addEleve(e);
+			return eleveRepository.findMean("Thomas");
+		}
+		else {
+			return -1;
+		}
 	}
 	
-	
+
 }
