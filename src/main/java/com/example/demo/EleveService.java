ae82b0980382f62df4c0666dab2a7dbfadd8d426
Victor Duval <victor.duval@epsi.fr>
2021-03-03 14:19:12 +0100

    ajout fonction

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -13,6 +13,17 @@ public class EleveService {
 	public boolean isPositiveNote(int note) {
 		return 0 <= note;
 	}
+
+	public int victor(int note) {
+        boolean isNote = this.isPositiveNote(note) && note <=20;
+        int mean =  -1;
+        if(isNote) {
+            Eleve a = new Eleve("victor", note);
+            eleveRepository.addEleve(a);
+            mean = eleveRepository.findMean("victor");
+        }
+        return mean;
+    }    
 	
 	
 }
