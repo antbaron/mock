7c5e110af5532cf10496ff5e93cebbb8c26a46bd
FlorentOddo <44065546+FlorentOddo@users.noreply.github.com>
2021-03-03 09:50:35 +0100

    add mock

--- src/test/java/com/example/demo/EleveServiceTest.java
+++ src/test/java/com/example/demo/EleveServiceTest.java
@@ -1,6 +1,14 @@
 package com.example.demo;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EleveServiceTest {
 
+	@Mock
+	private EleveRepository eleveRepository;
 	
+	@InjectMocks
+	private EleveService sut;
 }
