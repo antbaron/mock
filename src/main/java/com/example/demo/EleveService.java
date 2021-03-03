aec445c98a943cfbcff05584cf1ea02fca8fbf11
William <william.loree1@epsi.fr>
2021-03-03 15:04:19 +0100

    pour asgard

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -3,16 +3,31 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Eleve;
+import com.example.demo.EleveRepository;
+
 @Service
 public class EleveService {
 
 	@Autowired
 	private EleveRepository eleveRepository;
-	
+
 
 	public boolean isPositiveNote(int note) {
 		return 0 <= note;
 	}
-	
-	
+
+	//A comment
+	public int setNote(int note) {
+		boolean isNote = this.isPositiveNote(note) && note <= 20;
+		int mean = -1;
+		if(isNote) {
+			Eleve a = new Eleve("Martin", 16);
+			eleveRepository.addEleve(a);
+			mean = eleveRepository.findMean("Martin");
+
+		}
+			return mean;
+
+	}
 }
