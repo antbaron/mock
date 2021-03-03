98e910f8cbcfdc9362cd4e11c1e1f3b820b0583b
=Martin <=martingenereuxccl@gmail.com>
2021-03-03 14:44:45 +0100

    jsp frrrrrrrrr

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Eleve;
 import com.example.demo.EleveRepository;
 
 @Service
@@ -18,10 +19,15 @@ public class EleveService {
 	
 	//A comment
 	public int setNote(int note) {
-		if(isPositiveNote(note)) {
-			return note;
-		}else {
-			return 0;
+		boolean isNote = this.isPositiveNote(note) && note <= 20;
+		int mean = -1;
+		if(isNote) {
+			Eleve a = new Eleve("Martin", 16);
+			eleveRepository.addEleve(a);
+			mean = eleveRepository.findMean("Martin");
+			
 		}
+			return mean;
+		
 	}
 }
