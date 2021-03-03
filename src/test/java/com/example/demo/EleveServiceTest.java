069ee3aefdb88bb2b16d9ac04a9df403f7aa2714
ValentinKeetiz <valentin.moulin@keetiz.com>
2021-03-03 14:51:56 +0100

    commit 1

--- src/test/java/com/example/demo/EleveServiceTest.java
+++ src/test/java/com/example/demo/EleveServiceTest.java
@@ -1,8 +1,28 @@
 package com.example.demo;
 
-public class EleveServiceTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-	public int valentin(int note) {
-    		return note;
-    	}
+@ExtendWith(MockitoExtension.class)
+public class EleveServiceTest {
+@Mock
+private EleveRepository eleve;
+@InjectMocks
+private EleveService sut;
+
+@Test
+public void Test1() {
+    int note = 0;
+    Mockito.doNothing().when(eleve).addEleve(Mockito.any());
+    Mockito.doReturn(10).when(eleve).findMean(Mockito.anyString());
+    int resp = sut.setNote(note);
+    Assertions.assertThat(resp).isEqualTo(10);
+}
+
+
 }
