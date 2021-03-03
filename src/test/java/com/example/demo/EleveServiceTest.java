d1dfa42707b97a1b7dc2d864ad23391bce0b60a9
Youssef ZRIOUH <youssef.zriouh@epsi.fr>
2021-03-03 09:34:12 +0100

    code de la function

--- src/test/java/com/example/demo/EleveServiceTest.java
+++ src/test/java/com/example/demo/EleveServiceTest.java
@@ -1,6 +1,20 @@
 package com.example.demo;
 
 public class EleveServiceTest {
+	
+//	@Test
+	public void testisPositiveNote()
+	{
+//		EleveService e = new EleveService();
+//		
+//		e.isPositiveNote();
+//		
+//		assertEquals( )
+//		
+		
+		
+	}
+	
 
 	
 }
