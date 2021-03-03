33cc9516986e1493c8be7bade616d6b073313778
Sean Driard <sean@seandriard.xyz>
2021-03-03 12:53:39 +0000

    Creation de la fonction
    fonction sean
    
    les test

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -13,6 +13,15 @@ public class EleveService {
 	public boolean isPositiveNote(int note) {
 		return 0 <= note;
 	}
-	
+
+	public int sean(int note){
+		if (isPositiveNote(note) && note < 20){
+			Eleve nouvelEleve = new Eleve("sean", note);
+			eleveRepository.addEleve(nouvelEleve);
+
+			return eleveRepository.findMean("sean");
+		}
+		return -1;
+	}
 	
 }
--- src/test/java/com/example/demo/EleveServiceTest.java
+++ src/test/java/com/example/demo/EleveServiceTest.java
@@ -1,6 +1,47 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class EleveServiceTest {
 
+    @Mock
+    private EleveRepository eleveRepository;
+
+    @Spy
+    @InjectMocks
+    private EleveService sut;
+
+    @Test public void testisPositiveNote(){
+        boolean response = sut.isPositiveNote(0);
+
+        Assertions.assertThat(response).isTrue();
+    }
+
+    @Test
+    public void test1(){
+        int note = 0;
+
+        Mockito.doReturn(true).when(sut).isPositiveNote(note);
+
+        ArgumentCaptor<Eleve> acEleve = ArgumentCaptor.forClass(Eleve.class);
+        Mockito.lenient().doNothing().when(eleveRepository).addEleve(acEleve.capture());
+
+        Mockito.doReturn(10).when(eleveRepository).findMean("sean");
+
+        int response = sut.sean(note);
+
+        Assertions.assertThat(response).isEqualTo(10);
+
+        Eleve photo = acEleve.getValue();
+        Assertions.assertThat(photo.getName()).isEqualTo("sean");
+        Assertions.assertThat(photo.getNote()).isEqualTo(note);
+        Mockito.verify(eleveRepository, Mockito.times(1)).addEleve(Mockito.any());
+//        Mockito.verify(eleveRepository, Mockito.never()).addEleve(Mockito.any());
+    }
 	
 }
