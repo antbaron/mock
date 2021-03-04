34705b807673167af53d3ce554b39ae88e63b221
Arthur <arthur.audenaert@epsi.fr>
2021-03-04 01:14:08 +0100

    feat #artaudenaert : ajout fonction anthony avec la note <=20 dans la fonction ispositive + tests partiels

--- src/main/java/com/example/demo/EleveService.java
+++ src/main/java/com/example/demo/EleveService.java
@@ -11,8 +11,18 @@ public class EleveService {
 	
 
 	public boolean isPositiveNote(int note) {
-		return 0 <= note;
+		return 0 <= note && note <= 20;
 	}
 	
-	
+	public int anthony(int note) {
+		boolean isNote = this.isPositiveNote(note);
+		int mean = -1;
+		if(isNote) {
+			Eleve a = new Eleve("anthony", note);
+			eleveRepository.addEleve(a);
+			mean = eleveRepository.findMean("anthony");
+		} 
+
+		return mean;
+	}
 }
